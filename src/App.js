7dd36bb573b5363d1444ac493ff1b24ef68e4b0d
nala-he <hytdaisy@gmail.com>
2022-09-12 21:55:08 -0400

    updated the note in App.js to indicate the branch from A2

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h6>This is a branch from assignment 2</h6>
         <a
           className="App-link"
           href="https://reactjs.org"
