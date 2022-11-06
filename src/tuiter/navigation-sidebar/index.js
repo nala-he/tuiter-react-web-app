380afb7e24d9942b43ca4154e3ffb5e76ec54c43
nala-he <hytdaisy@gmail.com>
2022-11-06 17:08:30 -0500

    highlight profile in edit-profile page according to piazza post @689

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -7,7 +7,8 @@ import {Link} from "react-router-dom";
 const NavigationSidebar = () => {
     const {pathname} = useLocation();
     const paths = pathname.split('/');
-    const active = (paths.length > 2 && paths[2] !== '') ? paths[2] : 'home';
+    let active = (paths.length > 2 && paths[2] !== '') ? paths[2] : 'home';
+    active = active.includes('profile') ? 'profile' : active;
 
     return (
         <>
